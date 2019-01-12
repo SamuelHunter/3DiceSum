49428fea50f8698f3fd52f6c4ec1a47f57ea3646
SamuelHunter <38480476+SamuelHunter@users.noreply.github.com>
2019-01-11 23:55:12 -0800

    Update 3_dice_sum.py
    improved comments

--- 3_dice_sum.py
+++ 3_dice_sum.py
@@ -1,48 +1,55 @@
+# author:       Samuel Hunter
+# date:         1/11/19
+# description:  Geometric representation of the most probable sum of three dice
+
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
 import itertools
 from itertools import product, combinations
 import numpy as np
-from matplotlib.widgets import Slider
+
 
 #constants
-NUM_DICE = 3
-K_INIT = 8  #initial guess for most probable sum
-ROLL_MIN = 1
-ROLL_MAX = 6
-MIN_SUM = NUM_DICE * ROLL_MIN
-MAX_SUM = NUM_DICE * ROLL_MAX
+NUM_DICE    =   3                     #3 dice in the problem
+K_INIT      =   8                     #initial guess for most probable sum
+ROLL_MIN    =   1                     #smallest value a roll can produce
+ROLL_MAX    =   6                     #largest value a roll can produce
+MIN_SUM     =   NUM_DICE * ROLL_MIN   #(1,1,1) => 3
+MAX_SUM     =   NUM_DICE * ROLL_MAX   #(6,6,6) => 18
 
 #possible outcomes
-dice_rolls = range(ROLL_MIN, ROLL_MAX + 1) #range() excludes the last value
-possible_sums = range(MIN_SUM, MAX_SUM + 1) #(1,1,1) to (6,6,6)
-cart_prod = [dice_rolls, dice_rolls, dice_rolls]    #Cartesian Product of 3 dice rolls
-sample_space = list(itertools.product(*cart_prod))  #All possible triples (events)
-sums = [sum(tup) for tup in sample_space]   #Corresponding sums from each event
+dice_rolls      =   range(ROLL_MIN, ROLL_MAX + 1)             #range() excludes the last value
+possible_sums   =   range(MIN_SUM, MAX_SUM + 1)               #(1,1,1) to (6,6,6)
+cart_prod       =   [dice_rolls, dice_rolls, dice_rolls]      #Cartesian Product of 3 dice rolls
+sample_space    =   list(itertools.product(*cart_prod))       #all possible triples (events)
+sums            =   [sum(tup) for tup in sample_space]        #corresponding sums from each event
 
 #plane x + y + z = k
-plane_pts, other_pts, other_sums = [], [], []
-def genPlanePts(k):#solve for Z values at X,Y
+plane_pts, other_pts, other_sums = [], [], []   #split from sample_space into two groups: on the plane or not
+#returns set of coordinates for plane, as well as sorting points
+def genPlanePts(k):
     plane_pts.clear()
     other_pts.clear()
     other_sums.clear()
     X,Y = np.meshgrid(dice_rolls, dice_rolls)   #calculate X and Y as a grid of range 1-6
-    Z = -1*X + -1*Y + k    #solve for Z values at X,Y
-    for pt,s in zip(sample_space,sums):  #fetch all points that satisfy x+y+z=k
+    Z = -1*X + -1*Y + k                         #solve for Z values at X,Y
+    for pt,s in zip(sample_space,sums):         #fetch all points that satisfy x+y+z=k
         if s == k:
-            plane_pts.append(pt)    #all of their sums are k, no need to record
+            plane_pts.append(pt)                #all of their sums are k, no need to record
         else:
             other_pts.append(pt)
             other_sums.append(s)
     return (X,Y,Z)
 
+#returns new output of intersections and probability of that sum
 def updateText(k):
-    s = len(plane_pts)
+    s = len(plane_pts)  #number of intersections
     updated = str(s)
     updated += ' intersection'
     if s > 1:
-        updated += 's' #grammar plural
+        updated += 's'  #grammar plural
     updated += '\nP(' + str(int(k)) + ') = ' + str(round(s/ROLL_MAX**NUM_DICE,6))   #probability to 6 decimal places
     return updated
 
@@ -75,8 +82,8 @@ def replot(k):
     X,Y,Z = genPlanePts(k)
     surf = ax.plot_surface(X, Y, Z, alpha=0.2, color='m')
     #sample space of lattice points
-    sctr = ax.scatter(*zip(*other_pts), c=other_sums,  facecolor=other_sums, cmap=colmap, s=50, marker='o', alpha=0.6) #sum points not in plane
-    highlight = ax.scatter(*zip(*plane_pts), c='gold',  facecolor='gold', edgecolor='k', linewidth=1, s=250, marker='*')#highlights points in the plane in yellow
+    sctr = ax.scatter(*zip(*other_pts), c=other_sums, facecolor=other_sums,cmap=colmap, s=50, marker='o', alpha=0.6)    #points not in plane
+    highlight = ax.scatter(*zip(*plane_pts), c='gold', facecolor='gold', edgecolor='k', linewidth=1, s=250, marker='*') #highlights points in the plane as stars
     #output text
     txt.set_text(updateText(k))
     #plot guides
@@ -94,10 +101,11 @@ axk = plt.axes([0.2, 0.1, 0.6, 0.04], facecolor='w')
 sk = Slider(axk, 'k', MIN_SUM, MAX_SUM, valinit=K_INIT, valstep=1, valfmt="%i", color='gold')
 def update(val):
     k = sk.val
-    replot(k)
+    replot(k)   #redo plane and stars
     fig.canvas.draw_idle()
 sk.on_changed(update)
 
-mng = plt.get_current_fig_manager() 
+#prevents rescaling
+mng = plt.get_current_fig_manager()
 mng.window.resizable(False, False)
 plt.show()
